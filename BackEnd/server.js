7eab54ae1166835f09925f0c53fc72d0cc215ed6
unknown <g00381223@atu.ie>
2023-12-19 00:44:41 +0000

    first commit

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -1,18 +1,22 @@
 const express = require('express');
 const app = express();
 const port = 4000;
-const cors = require('cors');
+// const cors = require('cors');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
+
+const path = require('path');
+app.use(express.static(path.join(__dirname, '../build')));
+app.use('/static', express.static(path.join(__dirname, 'build//static')));
 // Use CORS for cross-origin requests
-app.use(cors());
-app.use(function (req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  next();
-});
+// app.use(cors());
+// app.use(function (req, res, next) {
+//   res.header('Access-Control-Allow-Origin', '*');
+//   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+//   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+//   next();
+// });
 
 // Configure express to use body-parser as middleware.
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -111,6 +115,12 @@ app.get('/api/tradingcard/:identifier', async (req, res) => {
     res.status(500).send('Internal Server Error');
   }
 });
+
+
+app.get('*', (req,res) =>{
+  res.sendFile(path.join(__dirname+'/../build/index.html'));
+  });
+
 
 // Express application to listen on the specified port
 app.listen(port, () => {
